284b30d648b50b545d5bf8a02b12fcb36a50cc7c
ghadakhaldi <khaldikhaldighada@gmail.com>
2021-11-28 18:42:52 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+          <h1 className="title red">Ghada</h1>
+          <br />
+          <img src="/imageInSrc.jpg" />
+          <br />
+          <img src="/imageInPublic.jpg" />
+        </div>
+        <video width={320} height={240} controls>
+          <source src="myVideo.mp4" type="video/mp4" />
+        </video><p />
+      </div>
+    );
+  }
+
+
+export default App;
